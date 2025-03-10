d95f35e5fe188bea266c0064690d9d2b0a399b03
Goldsand <158531180+G0ldsand01@users.noreply.github.com>
2025-03-09 21:13:00 -0400

    board

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
