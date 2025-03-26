6b0ffc56b3ca1f2a7d0a8df334e42f667fbb7bd3
Ecorte <me@ecorte.xyz>
2025-03-26 19:56:47 -0400

    authjs

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,24 @@
 import GitHub from '@auth/core/providers/github';
 import Discord from '@auth/core/providers/discord';
 import { defineConfig } from 'auth-astro';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import db from './src/lib/db';
+import type { Adapter } from '@auth/core/adapters';
+
+import {
+  usersTable,
+  accountsTable,
+  sessionsTable,
+  verificationTokensTable,
+} from './src/db/schema';
 
 export default defineConfig({
+  adapter: DrizzleAdapter(db, {
+    usersTable: usersTable,
+    accountsTable: accountsTable,
+    sessionsTable: sessionsTable,
+    verificationTokensTable: verificationTokensTable,
+  }) as Adapter,
   providers: [
     GitHub({
       clientId: import.meta.env.GITHUB_CLIENT_ID,
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,11 +1,14 @@
-import { integer, numeric, pgTable, text, varchar } from 'drizzle-orm/pg-core';
-
-export const usersTable = pgTable('users', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-});
+import {
+  boolean,
+  integer,
+  numeric,
+  pgTable,
+  primaryKey,
+  text,
+  timestamp,
+  varchar,
+} from 'drizzle-orm/pg-core';
+import type { AdapterAccount } from '@auth/core/adapters';
 
 export const productsTable = pgTable('products', {
   id: integer().primaryKey(),
@@ -16,3 +19,86 @@ export const productsTable = pgTable('products', {
   reviews: numeric().notNull(),
   image: varchar({ length: 255 }).notNull(),
 });
+
+export const usersTable = pgTable('user', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  name: text('name'),
+  email: text('email').unique(),
+  emailVerified: timestamp('emailVerified', { mode: 'date' }),
+  image: text('image'),
+});
+
+export const accountsTable = pgTable(
+  'account',
+  {
+    userId: text('userId')
+      .notNull()
+      .references(() => usersTable.id, { onDelete: 'cascade' }),
+    type: text('type').$type<AdapterAccount['type']>().notNull(),
+    provider: text('provider').notNull(),
+    providerAccountId: text('providerAccountId').notNull(),
+    refresh_token: text('refresh_token'),
+    access_token: text('access_token'),
+    expires_at: integer('expires_at'),
+    token_type: text('token_type'),
+    scope: text('scope'),
+    id_token: text('id_token'),
+    session_state: text('session_state'),
+  },
+  (account) => [
+    {
+      compoundKey: primaryKey({
+        columns: [account.provider, account.providerAccountId],
+      }),
+    },
+  ]
+);
+
+export const sessionsTable = pgTable('session', {
+  sessionToken: text('sessionToken').primaryKey(),
+  userId: text('userId')
+    .notNull()
+    .references(() => usersTable.id, { onDelete: 'cascade' }),
+  expires: timestamp('expires', { mode: 'date' }).notNull(),
+});
+
+export const verificationTokensTable = pgTable(
+  'verificationToken',
+  {
+    identifier: text('identifier').notNull(),
+    token: text('token').notNull(),
+    expires: timestamp('expires', { mode: 'date' }).notNull(),
+  },
+  (verificationToken) => [
+    {
+      compositePk: primaryKey({
+        columns: [verificationToken.identifier, verificationToken.token],
+      }),
+    },
+  ]
+);
+
+export const authenticatorsTable = pgTable(
+  'authenticator',
+  {
+    credentialID: text('credentialID').notNull().unique(),
+    userId: text('userId')
+      .notNull()
+      .references(() => usersTable.id, { onDelete: 'cascade' }),
+    providerAccountId: text('providerAccountId').notNull(),
+    credentialPublicKey: text('credentialPublicKey').notNull(),
+    counter: integer('counter').notNull(),
+    credentialDeviceType: text('credentialDeviceType').notNull(),
+    credentialBackedUp: boolean('credentialBackedUp').notNull(),
+    transports: text('transports'),
+  },
+  (authenticator) => [
+    {
+      compositePK: primaryKey({
+        columns: [authenticator.userId, authenticator.credentialID],
+      }),
+    },
+  ]
+);
