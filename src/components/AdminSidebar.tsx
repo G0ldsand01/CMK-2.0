ef7a21a34cf8122f73433faaa624e682d391b466
Ecorte <me@ecorte.xyz>
2025-04-02 09:26:57 -0400

    Update AdminSidebar.tsx

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -24,6 +24,7 @@ import {
   SettingsIcon,
   BellIcon,
   HomeIcon,
+  ShoppingCartIcon,
 } from 'lucide-react';
 import { UserMenu } from '@/components/UserMenu'; // Ensure the path is correct
 import type User from '@/lib/models/user';
@@ -38,6 +39,7 @@ const data = {
         {
           title: 'Home',
           url: '#',
+          icon: HomeIcon,
         },
       ],
     },
@@ -48,10 +50,12 @@ const data = {
         {
           title: 'Manage Users',
           url: '#',
+          icon: UsersIcon,
         },
         {
           title: 'Manage Roles',
           url: '#',
+          icon: UsersIcon,
         },
       ],
     },
@@ -62,26 +66,32 @@ const data = {
         {
           title: 'Manage Products',
           url: '#',
+          icon: ShoppingCartIcon,
         },
         {
           title: 'Manage Categories',
           url: '#',
+          icon: ShoppingCartIcon,
         },
         {
           title: 'Manage Orders',
           url: '#',
+          icon: ShoppingCartIcon,
         },
         {
           title: 'Manage Coupons',
           url: '#',
+          icon: ShoppingCartIcon,
         },
         {
           title: 'Manage Reviews',
           url: '#',
+          icon: ShoppingCartIcon,
         },
         {
           title: 'Manage Settings',
           url: '#',
+          icon: ShoppingCartIcon,
         },
       ],
     },
@@ -114,7 +124,10 @@ export function AppSidebar({
                 {item.items.map((item) => (
                   <SidebarMenuItem key={item.title}>
                     <SidebarMenuButton asChild>
-                      <a href={item.url}>{item.title}</a>
+                      <a href={item.url}>
+                        <item.icon size={16} />
+                        {item.title}
+                      </a>
                     </SidebarMenuButton>
                   </SidebarMenuItem>
                 ))}
