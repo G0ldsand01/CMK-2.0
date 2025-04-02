44cb2a14ef3e09e355d710f296f8870cf1b53179
Ecorte <me@ecorte.xyz>
2025-04-02 15:14:49 -0400

    Add compress and critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,16 @@ import tailwindcss from '@tailwindcss/vite';
 import dotenv from 'dotenv';
 import vercel from '@astrojs/vercel';
 
+import compress from 'astro-compress';
+
+import critters from 'astro-critters';
+
 dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
   // prefetch: true,
-  integrations: [react(), auth()],
+  integrations: [react(), auth(), critters(), compress()],
 
   output: 'server',
 
