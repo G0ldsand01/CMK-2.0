3303abbe6d4a70d51318fb8ee5626ee7b3ff8e4d
Ecorte <me@ecorte.xyz>
2025-04-03 18:11:08 -0400

    Some polishing

---
+++ src/pages/api/image/[image].png.ts
@@ -0,0 +1,25 @@
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async ({ params }) => {
+	const name = params.image;
+
+    console.log(name);
+
+	const imagePath = join(
+		process.cwd(),
+		'src',
+		'assets',
+		'images',
+		`${name}.png`,
+	);
+
+	const image = await readFile(imagePath);
+
+	return new Response(image, {
+		headers: {
+			'Content-Type': 'image/png',
+		},
+	});
+};
