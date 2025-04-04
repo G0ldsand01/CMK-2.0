77d1052e3ed7dbd0acf9e89bb1992c2b3fc31ece
Ecorte <me@ecorte.xyz>
2025-04-04 16:06:20 -0400

    Remove useless log

--- src/pages/api/image/[image].png.ts
+++ src/pages/api/image/[image].png.ts
@@ -5,8 +5,6 @@ import type { APIRoute } from 'astro';
 export const GET: APIRoute = async ({ params }) => {
 	const name = params.image;
 
-	console.log(name);
-
 	const imagePath = join(
 		process.cwd(),
 		'src',
